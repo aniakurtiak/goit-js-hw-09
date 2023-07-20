90f2364a4e304814b2583f449b167cf20cc82b52
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-21 01:53:34 +0300

    onClose

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,22 +1,20 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+const currentDate = new Date();
+
 
 flatpickr("#datetime-picker", {
     enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
+    onClose(selectedDates) {
+        for (let i = 0; i < selectedDates.length; i += 1) {
+            if (selectedDates[1] < currentDate) {
+                window.alert("Please choose a date in the future"); 
+          } 
+      }
     console.log(selectedDates[0]);
     }
 });

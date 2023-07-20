1de0b407465fa83c86eb6daecb365a5c058feb30
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-21 02:34:30 +0300

    currentDate

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,14 +2,17 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 
+
+
 flatpickr("#datetime-picker", {
     enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
     onClose(selectedDates) {
+         const currentDate = this.config.defaultDate;
         for (let i = 0; i < selectedDates.length; i += 1) {
-            if (selectedDates[0] < this.defaultDate) {
+            if (selectedDates[0] < currentDate) {
                 window.alert("Please choose a date in the future"); 
           } 
       }

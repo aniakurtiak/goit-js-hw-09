9f05a09541045a482454f9b28c0cb64e285f7561
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-21 01:32:14 +0300

    add-options2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,4 +11,12 @@ const options = {
   },
 };
 
-flatpickr("#datetime-picker", {options});
+flatpickr("#datetime-picker", {
+    enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    }
+});

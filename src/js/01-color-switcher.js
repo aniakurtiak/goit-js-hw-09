44494ac0a9ea2ac058dad474f969b7d6eb226ede
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-21 00:54:00 +0300

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+buttonStart.addEventListener('click', handlerStart);
+buttonStop.addEventListener('click', handlerStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let timerId = null;
+
+function handlerStart(evt) {
+  evt.target.disabled = true;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handlerStop() {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+}
 

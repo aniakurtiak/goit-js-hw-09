8e109702688c512e9215c87c051f86f533a81959
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-21 02:17:59 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,7 +9,7 @@ flatpickr("#datetime-picker", {
   minuteIncrement: 1,
     onClose(selectedDates) {
         for (let i = 0; i < selectedDates.length; i += 1) {
-            if (selectedDates[0] < defaultDate) {
+            if (selectedDates[0] < [defaultDate]) {
                 window.alert("Please choose a date in the future"); 
           } 
       }

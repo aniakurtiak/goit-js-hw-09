1800f33eed6b4e57368c07651d217d2ef37e5c3c
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-21 03:56:38 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,21 +5,18 @@ import "flatpickr/dist/flatpickr.min.css";
 const buttonStart = document.querySelector('[data-start]');
  buttonStart.disabled = true;
 
-let currentDate = new Date();
-function getCopyOfCurrentDate() {
-  const currentDate = new Date();
-  return new Date(currentDate.getTime());
-}
+// let currentDate = new Date();
+
 
 flatpickr("#datetime-picker", {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: getCopyOfCurrentDate(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
+   enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
         if (selectedDates[0] < currentDate) {
             window.alert("Please choose a date in the future");
-        } else if (selectedDates[0] > currentDate) {
+        } else if (selectedDates[0] > this.defaultDate) {
             buttonStart.disabled = false;
         }
     

0e8a8e370027f160b711b1448a44ceb952d6171d
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-21 03:40:29 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,28 +3,29 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const buttonStart = document.querySelector('[data-start]');
-//  buttonStart.disabled = true;
+ buttonStart.disabled = true;
 
 let currentDate = new Date();
 
 flatpickr("#datetime-picker", {
     enableTime: true,
-  time_24hr: true,
-  defaultDate: currentDate,
-  minuteIncrement: 1,
+    time_24hr: true,
+    defaultDate: currentDate,
+    minuteIncrement: 1,
     onClose(selectedDates) {
-    // if (selectedDates[0] < currentDate) {
-    //             window.alert("Please choose a date in the future"); 
-    //         } else if (selectedDates[0] > currentDate){
-    //             buttonStart.disabled = false;
-    //       }
+        if (selectedDates[0] < currentDate) {
+            window.alert("Please choose a date in the future");
+        } else if (selectedDates[0] > currentDate) {
+            buttonStart.disabled = false;
+        }
     
-    if (selectedDates[0] > currentDate) {
-      buttonStart.disabled = false; 
-    } else {
-      buttonStart.disabled = true;
-      window.alert("Please choose a date in the future");
-    }
-    console.log(selectedDates[0]);
+        // if (selectedDates[0] > currentDate) {
+        //   buttonStart.disabled = false; 
+        // } else {
+        //   buttonStart.disabled = true;
+        //   window.alert("Please choose a date in the future");
+        // }
+        // console.log(selectedDates[0]);
+        // }
     }
 });

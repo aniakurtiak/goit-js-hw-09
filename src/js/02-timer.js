2ad0936bf217b0efa28c9a21abf367b7e3ae5cd4
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-24 00:40:26 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,38 +8,26 @@ const datePicker = document.getElementById("datetime-picker");
       const minutesValue = document.querySelector("[data-minutes]");
       const secondsValue = document.querySelector("[data-seconds]");
 
- function addLeadingZero(value) {
-      return value.toString().padStart(2, '0');
-    }
+const options = {
+      enableTime: true,
+      time_24hr: true,
+      defaultDate: new Date(),
+      minuteIncrement: 1,
+      onClose(selectedDates) {
+        const selectedDate = selectedDates[0];
+        if (selectedDate < new Date()) {
+          window.alert("Please choose a date in the future");
+          startButton.disabled = true;
+        } else {
+          startButton.disabled = false;
+        }
+      },
+    };
 
-    function convertMs(ms) {
-      // Number of milliseconds per unit of time
-      const second = 1000;
-      const minute = second * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-
-      // Remaining days
-      const days = Math.floor(ms / day);
-      // Remaining hours
-      const hours = Math.floor((ms % day) / hour);
-      // Remaining minutes
-      const minutes = Math.floor(((ms % day) % hour) / minute);
-      // Remaining seconds
-      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-      return { days, hours, minutes, seconds };
-    }
+    flatpickr("#datetime-picker", options);
 
 document.addEventListener("click", handlerClick);
 function handlerClick(event) {
-      // const datePicker = document.getElementById("datetime-picker");
-      // const startButton = document.querySelector("[data-start]");
-      // const daysValue = document.querySelector("[data-days]");
-      // const hoursValue = document.querySelector("[data-hours]");
-      // const minutesValue = document.querySelector("[data-minutes]");
-      // const secondsValue = document.querySelector("[data-seconds]");
-
       if (event.target === startButton) {
         const selectedDate = new Date(datePicker.value).getTime();
 
@@ -66,22 +54,27 @@ function handlerClick(event) {
       }
     };
 
-    const options = {
-      enableTime: true,
-      time_24hr: true,
-      defaultDate: new Date(),
-      minuteIncrement: 1,
-      onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
-        const startButton = document.querySelector("[data-start]");
+    function convertMs(ms) {
+      // Number of milliseconds per unit of time
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
 
-        if (selectedDate < new Date()) {
-          window.alert("Please choose a date in the future");
-          startButton.disabled = true;
-        } else {
-          startButton.disabled = false;
-        }
-      },
-    };
+      // Remaining days
+      const days = Math.floor(ms / day);
+      // Remaining hours
+      const hours = Math.floor((ms % day) / hour);
+      // Remaining minutes
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      // Remaining seconds
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    flatpickr("#datetime-picker", options);
+      return { days, hours, minutes, seconds };
+    }
+
+
+
+     function addLeadingZero(value) {
+      return value.toString().padStart(2, '0');
+    }

8f1f87c6c271b1487a873e6182bca6c54e5f5d7a
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-24 00:28:11 +0300

    const

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,13 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const datePicker = document.getElementById("datetime-picker");
+      const startButton = document.querySelector("[data-start]");
+      const daysValue = document.querySelector("[data-days]");
+      const hoursValue = document.querySelector("[data-hours]");
+      const minutesValue = document.querySelector("[data-minutes]");
+      const secondsValue = document.querySelector("[data-seconds]");
+
  function addLeadingZero(value) {
       return value.toString().padStart(2, '0');
     }
@@ -24,13 +31,14 @@ import "flatpickr/dist/flatpickr.min.css";
       return { days, hours, minutes, seconds };
     }
 
-    document.addEventListener("click", function (event) {
-      const datePicker = document.getElementById("datetime-picker");
-      const startButton = document.querySelector("[data-start]");
-      const daysValue = document.querySelector("[data-days]");
-      const hoursValue = document.querySelector("[data-hours]");
-      const minutesValue = document.querySelector("[data-minutes]");
-      const secondsValue = document.querySelector("[data-seconds]");
+document.addEventListener("click", handlerClick);
+function handlerClick(event) {
+      // const datePicker = document.getElementById("datetime-picker");
+      // const startButton = document.querySelector("[data-start]");
+      // const daysValue = document.querySelector("[data-days]");
+      // const hoursValue = document.querySelector("[data-hours]");
+      // const minutesValue = document.querySelector("[data-minutes]");
+      // const secondsValue = document.querySelector("[data-seconds]");
 
       if (event.target === startButton) {
         const selectedDate = new Date(datePicker.value).getTime();
@@ -56,7 +64,7 @@ import "flatpickr/dist/flatpickr.min.css";
 
         updateTimer();
       }
-    });
+    };
 
     const options = {
       enableTime: true,
